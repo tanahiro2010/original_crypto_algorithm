3ca81239d9d7c47f36b7c2ca7cc1ef35ad97fec7
tanahiro2010 <herentongkegu087@gmail.com>
2026-02-13 22:15:43 +0900

    feat: XOR演算を実装する関数を追加

---
+++ src/lib/algos.py
@@ -0,0 +1,13 @@
+
+
+def xor(text: list[int], key: list[int]) -> int:
+    """Perform XOR operation between two integers.
+    Args:
+        text (int): The first integer.
+        key (int): The second integer.
+    Returns:
+        int: The result of the XOR operation.
+    """
+
+
+    return 

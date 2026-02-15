2fbd1befae852ae567f9bc5621cef9ba0e327066
tanahiro2010 <herentongkegu087@gmail.com>
2026-02-15 11:59:33 +0900

    feat: main関数にエンコード機能を追加

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import sys
+from crypto.index import encode, decode
 
 
 
 def main():
     content = sys.argv[1] if len(sys.argv) > 1 else "Default Content"
     print(f"Content: {content}")
+    encoded = encode(content)
 
     return
 
